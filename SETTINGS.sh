8d61860648eaa5606a600d3c65552a3eb208f914
Futafi <D.futagi@vivaldi.net>
2021-05-04 14:01:44 +0900

    WSL2でのDISPLAY環境変数に対応

--- SETTINGS.sh
+++ SETTINGS.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 VIVADO_PATH=/tools/Xilinx/Vivado;export VIVADO_PATH
 VIVADO_VERSION=2020.2;export VIVADO_VERSION
+if [ -e "/proc/sys/fs/binfmt_misc/WSLInterop" ]; then
+	export DISPLAY=$(cat /etc/resolv.conf | grep nameserver | awk '{print $2}'):0.0
+fi
